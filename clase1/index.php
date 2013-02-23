40b1ceb3027f5a6fc7a4d1e2984d5676b3f866f1
Andrey <andrey_201015@hotmail.com>
2013-02-23 12:18:00 -0500

    Subiendo Archivos clase1

---
+++ clase1/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Ejemplo de grip</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/bootstrap-responsive.css">
+	<script src="js/bootstrap.js"></script>
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row-fluid">
+			<div class="span4">
+				<div class="well">Hola</div>
+			</div>
+			<div class="span4">
+				<div class="well">COmo estas</div>
+			</div>
+			<div class="span4">
+				<div class="well"> JOhn</div>
+			</div>
+		</div>
+	</div>
+	<hr>
+	<div class="container">
+		<div class="row">
+			<div class="span4">
+				<div class="well">menu izquierdo</div>
+			</div>
+			<div class="span8">
+				<div class="well">contenido</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
