7b28b504748c78d4d23b4ce2c1204a64bb44ae21
Andrey <andrey_201015@hotmail.com>
2013-02-27 22:02:57 -0500

    Subiendo Archivos Clase2

---
+++ clase2/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Clase 2</title>
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<script src="js/bootstrap.js"></script>
+</head>
+<body>
+	<hr>
+	<article class="container">
+	   <form class="form-horizontal">
+	   	  <div class="control-group">
+	   	  	<label class="control-label">Email</label>
+	   	  	<div class="controls">
+	   	  		<input type="text" placeholder="Email">
+	   	  	</div>
+	   	  </div>
+	   	  <div class="control-group">
+	   	  	<label class="control-label">Contraseña</label>
+	   	  	<div class="controls">
+	   	  		<input type="password" placeholder="Contraseña">
+	   	  	</div>
+	   	  </div>
+	   	  <div class="controls">
+	   	  	 <label class="checkbox">
+	   	  	 	<input type="checkbox">Recordarme
+	   	  	 </label>
+	   	     <button type="submit" class="btn">Iniciar</button>
+	   	  </div>
+	   </form>
+	</article>
+</body>
+</html>
