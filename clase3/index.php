e936e44b004c9e058f59e4e229f7195349ccfb5c
Andrey <andrey_201015@hotmail.com>
2013-03-24 21:44:48 -0500

    Subiendo Clase3
    :)

---
+++ clase3/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Clase 3</title>
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<script src="js/jquery.js"></script>
+	<script src="js/bootstrap.js"></script>
+</head>
+<body>
+	<hr>
+	<article class="container">
+	    <div class="btn-group">
+	    	<a href="#" class="btn btn-primary"><i class="icon-user icon-white"></i>Usuario</a>
+	    	<a href="#" class="btn btn-primary dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></a>
+	    	<ul class="dropdown-menu">
+	    		<li><a href=""><i class="icon-pencil"></i>Editar</a></li>
+	    		<li><a href=""><i class="icon-trash"></i>Eliminar</a></li>
+	    		<li><a href=""><i class="icon-ban-circle"></i>Bloquear</a></li>
+	    		<li class="divider"></li>
+	    		<li><a href=""><i class="i"></i>Administrador</a></li>
+	    	</ul>
+	    </div>
+	</article>
+</body>
+</html>
