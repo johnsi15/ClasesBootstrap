0a3fdf5a5c3f23692759afcd4c179469958f6b57
John Andrey <andrey_201015@hotmail.com>
2013-05-13 18:20:15 -0500

    Subiendo Clase 4
    Los archivos de la presentacion estan hay

---
+++ clase4/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE HTML>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Clase 4</title>
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/bootstrap-responsive.css">
+	<script src="js/jquery.js"></script>
+	<script src="js/bootstrap.js"></script>
+</head>
+<body>
+	<header>
+		<div class="navbar navbar-fixed-top">
+			<div class="navbar-inner">
+				<div class="container">
+					<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</a>
+					<a href="" class="brand">Titulo</a>
+					<div class="nav-collapse collapse">
+						<ul class="nav">
+							<li class="active"><a href="#">Home</a></li>
+							<li><a href="">Noticias</a></li>
+							<li><a href="">Contactanos</a></li>
+							<form action="" class="navbar-search pull-left">
+								<input type="text" class="search-query" placeholder="Buscar">
+							</form>
+							<li class="divider-vertical"></li>
+							<li class="dropdown">
+								<a href="" class="dropdown-toggle" data-toggle="dropdown">
+									Archivos
+									<p class="caret"></p>
+								</a>
+								<ul class="dropdown-menu">
+									<li><a href="">Empresa</a></li>
+									<li><a href="">Proveedor</a></li>
+									<li><a href="">Cajas</a></li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+	<article class="container">
+		
+	</article>
+	<article class="container">
+		<p>Inicio</p>
+	</article>
+</body>
+</html>
